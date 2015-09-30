71ee54760edd384792975968fefd4e6772eb9f7a
Widespace <dawnlightsmile@yahoo.com>
2015-09-30 16:15:43 -0400

    clear detail area when changing categories

--- webplus-dashboard/includes/interactive_map.js
+++ webplus-dashboard/includes/interactive_map.js
@@ -1,13 +1,9 @@
-// JavaScript Document
-// Written by Chris Converse
+// modeled on code by Chris Converse
 // for Lynda.com
 
 $(document).ready(function() {
-// begin Ready
 
-	//...................................................
-	// When the form changes
-	$('#mapForm').change(function() {
+	$('#mapForm').change(function() {	// When the form changes
 	
 		var selectedContinent = $('#mapForm option:selected').val();
 		if (selectedContinent == 'ALL'){
@@ -17,11 +13,25 @@ $(document).ready(function() {
 			$('a.dot[continent != "'+selectedContinent+'"]').slideUp(1000);
 		}
 		
+	//	Clear city detail area and replace with prompt msg
+	// in :: $('.detail_container .city_detail').html( :: jQuery  does not allow breaking the argument literal into multiple lines
+	//                                                     also: Requires doublequote wrapper, ticks inside the liter where quotes are needed
+	//													::
+		$('.detail_container').fadeOut(500, function(){
+			$('.detail_container .city_detail').html(
+				"<img width='75' class='city_photo' alt='~' src='localResources/images/nothing.png'><div class='city_info'><h3>Click</h3></div>");
+			$('.detail_container').fadeIn(500);
+		});
+
 	});
 	
 	//...................................................
 	// When a dot is clicked
 	$('a.dot').click(function(){
+
+//~ $(this) ==the instance of a class seelcted
+//~ $(a,dot) ==applied to the entire class
+//~ alert($(this).attr('city') + '~' + $(this).attr('Continent')) //dot attributes
 
 		$('a.dot').removeClass('selected');
 		$(this).addClass('selected');
@@ -32,7 +42,9 @@ $(document).ready(function() {
 		$('.detail_container').fadeOut(500, function(){
 			$('.detail_container .city_detail').html(htmlCode);
 			$('.detail_container').fadeIn(500);
+
 		});
+
 
 	});
 
