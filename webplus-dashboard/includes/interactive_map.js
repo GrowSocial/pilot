9a0dcb725bb17e2b040a02b0c231dd585bc5809e
Widespace <dawnlightsmile@yahoo.com>
2015-09-30 17:15:10 -0400

    Revert "clear detail area when changing categories"
    This reverts commit 71ee54760edd384792975968fefd4e6772eb9f7a.

--- webplus-dashboard/includes/interactive_map.js
+++ webplus-dashboard/includes/interactive_map.js
@@ -1,9 +1,13 @@
-// modeled on code by Chris Converse
+// JavaScript Document
+// Written by Chris Converse
 // for Lynda.com
 
 $(document).ready(function() {
+// begin Ready
 
-	$('#mapForm').change(function() {	// When the form changes
+	//...................................................
+	// When the form changes
+	$('#mapForm').change(function() {
 	
 		var selectedContinent = $('#mapForm option:selected').val();
 		if (selectedContinent == 'ALL'){
@@ -13,25 +17,11 @@ $(document).ready(function() {
 			$('a.dot[continent != "'+selectedContinent+'"]').slideUp(1000);
 		}
 		
-	//	Clear city detail area and replace with prompt msg
-	// in :: $('.detail_container .city_detail').html( :: jQuery  does not allow breaking the argument literal into multiple lines
-	//                                                     also: Requires doublequote wrapper, ticks inside the liter where quotes are needed
-	//													::
-		$('.detail_container').fadeOut(500, function(){
-			$('.detail_container .city_detail').html(
-				"<img width='75' class='city_photo' alt='~' src='localResources/images/nothing.png'><div class='city_info'><h3>Click</h3></div>");
-			$('.detail_container').fadeIn(500);
-		});
-
 	});
 	
 	//...................................................
 	// When a dot is clicked
 	$('a.dot').click(function(){
-
-//~ $(this) ==the instance of a class seelcted
-//~ $(a,dot) ==applied to the entire class
-//~ alert($(this).attr('city') + '~' + $(this).attr('Continent')) //dot attributes
 
 		$('a.dot').removeClass('selected');
 		$(this).addClass('selected');
@@ -42,9 +32,7 @@ $(document).ready(function() {
 		$('.detail_container').fadeOut(500, function(){
 			$('.detail_container .city_detail').html(htmlCode);
 			$('.detail_container').fadeIn(500);
-
 		});
-
 
 	});
 
