4ab1a0c7bb8e0ea67a8bc33e6acd1afd5735e751
Widespace <dawnlightsmile@yahoo.com>
2015-10-15 16:14:24 -0400

    Meteor Member and Business collection core modules

---
+++ autoform-SysadminUtl/server/security.js
@@ -0,0 +1,29 @@
+People.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
+Bizness.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
+Items.allow({
+  update: function () {
+    return true;
+  }
+});
+
+
