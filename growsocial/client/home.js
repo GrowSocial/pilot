4940b76430f353afac046f7e090fa607ef99f8f0
Misha Verplak <mrverplak@gmail.com>
2015-10-22 11:15:04 -0400

    add button event for my profile

--- growsocial/client/home.js
+++ growsocial/client/home.js
@@ -1,14 +1,8 @@
-Template.leafButtons.helpers({
-  leafPushed: function () {
-    return Session.get('leafPushed');
-  }
-});
-
 Template.leafButtons.events({
   'click button': function (event) {
-    // Session.set('leafPushed', event.target.id);
-    console.log('leafButtons click button: ', event.target.id);
-    if (event.target.id === "membersButton") {
+    if (event.target.id === "myProfileButton") {
+      Router.go("/memberProfile");
+    } else if (event.target.id === "membersButton") {
       Router.go("/memberProfile");
     } else if (event.target.id === "localBusinessButton") {
       Router.go("/localBusiness");
