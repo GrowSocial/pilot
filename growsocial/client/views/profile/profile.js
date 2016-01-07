57d45bfeeebebe09f289351ad1be0a690c87b930
Widespace <dawnlightsmile@yahoo.com>
2016-01-07 17:17:36 -0500

    eliminate member_id from edit schema

--- growsocial/client/views/profile/profile.js
+++ growsocial/client/views/profile/profile.js
@@ -1,3 +1,5 @@
+
+
 UI.registerHelper('formatTime', function(context, options) {
   if(context)
     return moment(context).format('MM/DD/YYYY, hh:mm');
--- growsocial/common/models/lib/PeopleModel.js
+++ growsocial/common/models/lib/PeopleModel.js
@@ -4,7 +4,38 @@ Meteor.isClient && Template.registerHelper("Schemas", Schemas);
 
 
 Schemas.Person = new SimpleSchema({
-  member_key: {type: String, index: 1,  unique: true },
+  //member_key: {type: String, index: 1,  unique: true },
+  email: {     type: String, optional: false,  unique: true },
+  firstname: { type: String, optional: true},
+  lastname: {  type: String, optional: true},
+  fullname: {  type: String, optional: true},
+  street: {    type: String, optional: true},
+  street2: {   type: String, optional: true},
+  city: {      type: String, optional: true},
+  state: {     type: String, optional: true ,
+    allowedValues: ["AL","AK","AZ","AR","CA","CO","CT","DE","FL",
+    "GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA",
+    "MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
+    "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA",
+    "WA","WV","WI","WY"],
+    autoform: { afFieldInput: { firstOption: "(Select a State)"  } }
+    },
+  zipcode: {type: String, optional: true },
+  occupations: {  type: Array,    optional: true  },    
+        'occupations.$': {       type: Object  },
+        'occupations.$.vocation': {type: String },
+        'occupations.$.descript': {type: String, optional:true },
+  location:{   type: String,   optional: true },
+  phone:{      type: String,   optional: true },
+  website:{    type: String,   optional: true },
+  links:{      type: String,   optional: true },
+  facebookID:{ type: String,   optional: true },
+  twitterID:{  type: String,   optional: true },
+  instagramID:{type: String,   optional: true },
+  about:{      type: String,   optional: true }
+});
+
+Schemas.profileEdit = new SimpleSchema({
   email: {     type: String, optional: false,  unique: true },
   firstname: { type: String, optional: true},
   lastname: {  type: String, optional: true},
@@ -42,6 +73,7 @@ Meteor.isClient && Template.registerHelper("Collections", Collections);
 
 People = Collections.People = new Mongo.Collection("People");
 People.attachSchema(Schemas.Person);
+//People.attachSchema(Schemas.profileEdit);
 
 //People = new Mongo.Collection('People');
 
