4a3a18a5d1c44477683beab134fac7570c9baaa4
Misha Verplak <mrverplak@gmail.com>
2016-01-12 02:17:44 +1100

    Send email after store message from contactUs

--- growsocial/common/methods/contactUsMethods.js
+++ growsocial/common/methods/contactUsMethods.js
@@ -10,5 +10,21 @@ Meteor.methods({
       createdAt: new Date(),
       authorId: Meteor.userId(), // can be null, meaning a message from someone not logged in
     });
+    
+    var text = 'name: ' + message.name + 
+      '\nemail: ' + message.email +
+      '\nzip: ' + message.zip +
+      '\ntext: ' + message.text;
+
+    // send an email containing the message
+    var email = {
+      to: "growsocial.org@gmail.com",
+      from: "email@growsocial.org",
+      subject: "GrowSocial website message - Contact Us",
+      text: text,
+    };
+    // TODO handle errors -> log into database, already done in email function?
+    Meteor.call('sendEmail', email, function(error, result) {});
+
   },
 });
