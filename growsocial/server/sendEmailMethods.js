962f3ee512dc82ae3bcb6a3bb9cf59b6c89956bd
Misha Verplak <mrverplak@gmail.com>
2016-01-12 13:36:31 +1100

    Simplify email method

--- growsocial/server/sendEmailMethods.js
+++ growsocial/server/sendEmailMethods.js
@@ -1,4 +1,5 @@
 Meteor.methods({
+  // TODO use callback to pass error/result to client
   sendEmail: function (email) {
     //Check to make sure fields are strings
     check([email.to, email.from, email.subject, email.text], [String]);
@@ -6,25 +7,24 @@ Meteor.methods({
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
     this.unblock();
-    if (Meteor.isServer) {
-      //Send email
-      try {
-        Email.send({
-          to: email.to,
-          from: email.from,
-          subject: email.subject,
-          text: email.text,
-        });
-        console.log("email sent");
-      }
-      // Catch any errors
-      catch(err) {
-        console.log("Error: ", err);
-        Meteor.call('addErrorLog', {
-          tag: 'sendEmail',
-          message: err.message
-        });
-      }
+    
+    //Send email
+    try {
+      Email.send({
+        to: email.to,
+        from: email.from,
+        subject: email.subject,
+        text: email.text,
+      });
+      console.log("email sent");
+    }
+    // Catch any errors
+    catch(err) {
+      console.log("Error: ", err);
+      Meteor.call('addErrorLog', {
+        tag: 'sendEmail',
+        message: err.message
+      });
     }
   }
 });
