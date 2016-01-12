50a596b40849718f1c95b4376f1c776b0f53b223
Misha Verplak <mrverplak@gmail.com>
2016-01-12 16:19:16 +1100

    Only send email if contactUs message inserted

--- growsocial/server/contactUsMethods.js
+++ growsocial/server/contactUsMethods.js
@@ -1,7 +1,6 @@
-// TODO feedback to user when message has reached server, not just mini mongo
-
 Meteor.methods({
-  addContactUsMessage: function(message) {
+  addContactUsMessage: function(message, callback) {
+    
     ContactUsMessages.insert({
       name: message.name,
       email: message.email,
@@ -9,22 +8,33 @@ Meteor.methods({
       text: message.text,
       createdAt: new Date(),
       authorId: Meteor.userId(), // can be null, meaning a message from someone not logged in
+    }, function(err) {
+      
+      if (err) {
+        console.log("ContactUsMessages.insert error: ", err);
+        Meteor.call('addErrorLog', {
+          tag: 'ContactUsMessages.insert',
+          message: err.message + ', message: ' + EJSON.stringify(message).substring(1,300),
+        });
+        callback && callback(err); // error response to client
+        
+      } else { // no err
+        
+        // send an email to ourselves containing the message
+        var text = 'name: ' + message.name + 
+          '\nemail: ' + message.email +
+          '\nzip: ' + message.zip +
+          '\ntext: ' + message.text;
+        var email = {
+          to: "GrowSocial Pilot Coordinator <growsocial.org@gmail.com>",
+          from: "GrowSocial Pilot Website <email@growsocial.org>",
+          subject: "GrowSocial pilot message - Contact Us",
+          text: text,
+        };
+        // no need to take handle email errors here
+        Meteor.call('sendEmail', email);
+        callback && callback(); // positive response to client
+      }
     });
-    
-    var text = 'name: ' + message.name + 
-      '\nemail: ' + message.email +
-      '\nzip: ' + message.zip +
-      '\ntext: ' + message.text;
-
-    // send an email to ourselves containing the message
-    var email = {
-      to: "GrowSocial Pilot Coordinator <growsocial.org@gmail.com>",
-      from: "GrowSocial Pilot Website <email@growsocial.org>",
-      subject: "GrowSocial pilot message - Contact Us",
-      text: text,
-    };
-    // TODO handle errors -> log into database, already done in email function?
-    Meteor.call('sendEmail', email, function(error, result) {});
-
   },
 });
