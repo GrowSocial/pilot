0e9ea58c969c4ae379012b7b199641edbaaa53e3
Misha Verplak <mrverplak@gmail.com>
2016-01-19 15:08:10 +1100

    Fix to disqus config

--- growsocial/client/views/discuss/discuss.js
+++ growsocial/client/views/discuss/discuss.js
@@ -4,29 +4,26 @@
 //  See https://help.disqus.com/customer/portal/articles/472107-using-disqus-on-ajax-sites
 
 var disqus_config = function () {
-  this.page.url = "//growsocial.meteor.com/discuss";  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.url = "https://growsocial.meteor.com/discuss";  // Replace PAGE_URL with your page's canonical URL variable
   this.page.identifier = "GROWSOCIAL-PILOT-DISCUSS"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+  this.page.title = 'General Discussion'; // a unique title for each page where Disqus is present
 };
 
 Template.discuss.onRendered(function() {
-  var template = this;
-  
   Session.set("loadDisqus", true);
-  
+
   if (window.DISQUS) {
     DISQUS.reset({
       reload: true,
-      config: function () {  
-        this.page.url = "//growsocial.meteor.com/discuss";
-        this.page.identifier = "GROWSOCIAL-PILOT-DISCUSS";
-      }
+      config: disqus_config,
     });
   }
 });
 
 Meteor.autorun(function() {
-  // # Load the Disqus embed.js the first time the template is rendered
-  // # but never more than once
+  // Load the Disqus embed.js the first time the template is rendered
+  // but never more than once
+  
   if (Session.get("loadDisqus") && !window.DISQUS) {
     (function() {  // DON'T EDIT BELOW THIS LINE
       var d = document, s = d.createElement('script');
