93524d9323b934dfce37202a969fdaf2415dd03e
Widespace <dawnlightsmile@yahoo.com>
2016-01-27 22:26:39 -0500

    fixing issue in connect feature and hide option for editors

--- growsocial/common/models/Connections.Model.js
+++ growsocial/common/models/Connections.Model.js
@@ -7,7 +7,8 @@ Schemas = {};
 Meteor.isClient && Template.registerHelper("Schemas", Schemas);
 
 
-*/
+FIRST MODEL::(preferred)  But not able to impliment as of Jan 2016, and replacing with 
+alternate version below.. /TEC
 
  Schemas.connectRec = new SimpleSchema({
          member_key:{ type: String,index: 1},
@@ -17,29 +18,14 @@ Meteor.isClient && Template.registerHelper("Schemas", Schemas);
     'contact.$.timestamp': {type: Date, optional:true },
     'contact.$.descript': {type: String, optional:true },
 });
+
+*/
+
+ Schemas.connectRec = new SimpleSchema({
+         member_key:{type: String,index: 1,optional: false },
+            contact:{type: String, optional:false},
+          timestamp:{type: Date, optional:false },
+           descript:{type: String, optional:false },
+});
 Connections.attachSchema(Schemas.connectRec);
 
-
-/*
-
-MarketItems2 = new Mongo.Collection("MarketItems2");
- 
- Schemas.MarketItems2 = new SimpleSchema({
-         member_key:{ type: String,index: 1},
-
-             items: { type: Array,    optional: false  },    
-               'items.$': { type: Object  },
-     'items.$.productId': { type: String , optional:true},
-          'items.$.member_id': { type: String , optional:true},
-   'items.$.description': { type: String , optional:true},
-          'items.$.type': { type: String , optional:true },
-    'items.$.salesAlert': { type: String , optional:true },
-      'items.$.unitType': { type: String , optional:true },
-     'items.$.unitPrice': { type: Number , optional:true, decimal: true },
-      'items.$.currency': { type: String , optional:true },
-         'items.$.photo': { type: Object , optional:true },  // url
-           'items.$.pic': { type: String , optional:true },
-  'items.$.date_entered': { type: Date, optional:true},
-});
-MarketItems2.attachSchema(Schemas.MarketItems2);
-*/
