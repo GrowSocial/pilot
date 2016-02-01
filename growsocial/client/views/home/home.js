5548a4e67ba889c39aa6989bb32a4ec7e360d853
nomad1111 <filiz015@gmail.com>
2016-01-31 22:19:49 -0500

    added member image, added helper for random member image updated links, tutorial text, donate button and open to new page

--- growsocial/client/views/home/home.js
+++ growsocial/client/views/home/home.js
@@ -22,8 +22,6 @@ Template.homeNew.onDestroyed(function() {
 });
 
 
-
-
 Template.homeNew.helpers({
   pathForProfile: function() {
     var params = {
@@ -32,4 +30,12 @@ Template.homeNew.helpers({
     var path = FlowRouter.path("profile", params);
     return path;
   },
+  pickMemberImage: function() {
+    return Random.choice([
+      "/images/member_image_block1.jpg",
+      "/images/member_image_block2.png",
+      "/images/member_image_block3.jpg",
+    ]);
+  },
 });
+
