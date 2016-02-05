ccbb0f0d9733d58d2ac375ce47e151a01bd3be65
Misha Verplak <mrverplak@gmail.com>
2016-02-05 16:28:45 +1100

    Moved payVendor to method

--- growsocial/client/views/cart/cart.js
+++ growsocial/client/views/cart/cart.js
@@ -34,94 +34,9 @@ Template.cart.events({
 
   // Pay to specific vendor
   'click .payVendor': function(event) {
-    var order = this.order;
-    var itemsPaid = "";
-    for (var i = 0, len = order.products.length; i < len; i++) {
-      itemsPaid += "- " + order.products[i].name + " (" + order.products[i].quantity + " " + order.products[i].unitType + ")\n";
-    }
-    console.log('itemsPaid',itemsPaid);
-    var email = {
-      to: order.vendorEmail,  // "seller@example.com",
-      from: "GrowSocial Pilot Website <growsocial.org@gmail.com>",
-      subject: "You have received a payment",
-      text: "The following items have been paid:\n" + itemsPaid,
-    }
-
-    if (Meteor.user()) {
-      email.text = "Buyer: " + Meteor.user().profile.firstname + " " +
-        Meteor.user().profile.lastname + 
-        ".\n" + email.text;
-    }
-    
-    Meteor.call('sendEmail', email);
-
-    if (Meteor.user()) {
-      email.to = Meteor.user().emails[0].address;  // "buyer@example.com",
-      email.subject = "You have made a payment";
-      email.text = "Seller: " + order.vendorName + "\nYou paid for the following: " + itemsPaid;
-    }
-    
-    Meteor.call('sendEmail', email);
-    
-    // prepare notification object
-    var notification = {
-      targetUserId: '' + order.vendorUserId, // ensure a string
-      tag: "Order",
-      html: true,
-      imageUrl: "/images/icons/dollar.png",
-      subject: "Order placed for my market items",
-      message: "Buyer: " + Meteor.user().profile.firstname + " " +
-        Meteor.user().profile.lastname + ". The following items have been paid:\n" + itemsPaid,
-    };
-    var error = {};
-    
-    // account for when buyer is not logged in
-    if (Meteor.user()) {
-      notification.senderUserId = Meteor.userId();
-      notification.sender = Meteor.user().profile.firstname;
-      notification.senderLastName = Meteor.user().profile.lastname;
-      error.email = Meteor.user().emails[0].address;
-      error.firstName = Meteor.user().profile.firstname;
-      error.lastName = Meteor.user().profile.lastname;
-    } else {
-      notification.sender = 'Anonymous';
-      error.firstName = 'Anonymous';
-    }
-    
-    // console.log('first notification', notification);
-    // first notification to the seller
-    if (order.vendorUserId) { // no point sending a notification to a null person
-      Meteor.call("addNotification", notification, function(err, result) {
-        if (err) {
-          error.tag = "PayVendorOrderNotification";
-          error.message = err.message;
-          error.errNumber = err.error;
-          Meteor.call("addErrorLog", error);
-        }
-      });
-    }
-
-    // prepare second notification    
-    if (Meteor.user()) {  // only notify if logged in!
-      notification.targetUserId = Meteor.userId();
-      notification.senderUserId = Meteor.userId();
-      notification.sender = Meteor.user().profile.firstname;
-      notification.senderLastName = Meteor.user().profile.lastname;
-
-      // second notification to the buyer    
-      notification.subject = "My order placed for market items";
-      notification.sender = "System";
-      notification.message = "Seller: " + order.vendorName + ".\nThe following items have been paid:\n" + itemsPaid;
-      // console.log('second notification', notification);
-      Meteor.call("addNotification", notification, function(err, result) {
-        if (err) {
-          error.tag = "MyOrderNotification";
-          error.message = err.message;
-          error.errNumber = err.error;
-          Meteor.call("addErrorLog", error);
-        }
-      });
-    }
+    event.preventDefault();
+    Meteor.call('payVendor',this.order);
+    // TODO better response to user on error/correct payment
   },
 
   'click .increase': function(event) {
